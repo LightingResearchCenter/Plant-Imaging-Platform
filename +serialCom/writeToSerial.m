acf99faa8e997b3ecbb947525c96b8e90a022dbc
plummt <plummt@rpi.edu>
2016-08-16 14:50:55 -0400

    Removing Pause Statements
    This brought down the time to run the program to under 20 minutes and is
    the most efficacy before pathing optimization.

--- +serialCom/writeToSerial.m
+++ +serialCom/writeToSerial.m
@@ -2,9 +2,9 @@ function output = writeToSerial(sPort,text)
 fullText = serialCom.str2com(text);
 fwrite(sPort,fullText);
 if strcmpi(sPort.Tag,'MSP432')
-    while (sPort.BytesAvailable < 1)
-        pause(0.5)
-    end
+%     while (sPort.BytesAvailable < 1)
+%         pause(0.5)
+%     end
 end
 output = fscanf(sPort);
 if strcmpi(sPort.Tag,'MSP432')
